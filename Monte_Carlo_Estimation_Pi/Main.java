3063a8f29c8bb8c7987109447a8593d253de7038
at1998 <43177479+at1998@users.noreply.github.com>
2019-02-02 12:01:43 +0530

    Update Main.java

--- Monte_Carlo_Estimation_Pi/Main.java
+++ Monte_Carlo_Estimation_Pi/Main.java
@@ -1,1 +1,39 @@
 
+
+package piestimation_montecarlo;
+
+/**
+ *
+ * @author Ayush
+ */
+import java.util.Random;
+import java.util.*;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int epochs=10000;
+     int i=1;
+     int r=1;
+     double incir=0;
+    double gen=0;
+     double pi_e=0;
+     double dist=0.0;
+     while(i<epochs)
+     {
+     double x=Math.random();
+     double y=Math.random();
+     dist=(x*x)+y*y;
+     if(dist<=1)
+         incir+=1;
+     gen++;
+     pi_e=(4*incir)/gen;
+     System.out.println("Iteration "+i+" :"+x+" "+y+" "+pi_e);
+     i+=1;
+
+
+    }
+
+}}
